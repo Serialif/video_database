e36b32cbbd302ed8cdfaeb7dbf9d53a0ef4dfdae
Serialif <contact@serialif.com>
2021-08-06 17:46:00 +0200

    Add excerpt Twig Function

---
+++ src/Twig/TextExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class TextExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            new TwigFilter('filter_name', [$this, 'doSomething']),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('excerpt', [$this, 'excerpt']),
+        ];
+    }
+
+    public function excerpt(string $text, ?int $length = 50): string
+    {
+        if (strlen($text) <= $length){
+            return $text;
+        }
+        return substr($text,0, $length) . '...';
+    }
+}
