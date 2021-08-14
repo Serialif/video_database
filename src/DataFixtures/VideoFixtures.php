afa792e2bcad99476e11603ffeb5b9a4d6184421
Serialif <contact@serialif.com>
2021-08-14 10:00:20 +0200

    Add tags in VideoFixtures and main template

--- src/DataFixtures/VideoFixtures.php
+++ src/DataFixtures/VideoFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\DataFixtures\Config\FixturesConfig;
+use App\Entity\Tag;
 use App\Entity\Video;
 use DateTimeImmutable;
 use Doctrine\Persistence\ObjectManager;
@@ -24,9 +25,30 @@ class VideoFixtures extends AbstractFixtures
             $video->setUrl($url);
             $video->setCreatedAt($date);
 
+            $tagRepository = $manager->getRepository(Tag::class);
+
+            $tags = $tagRepository->findAll();
+
+            $tagsNumber = rand(0, count($tags));
+            $usedTags = [];
+
+            for ($i = 0; $i < $tagsNumber; $i++) {
+                while (count($usedTags) < $tagsNumber) {
+                    $tagNumber = rand(0, count($tags) - 1);
+                    if (!in_array($tagNumber, $usedTags)) {
+                        $usedTags[] = $tagNumber;
+                        $video->addTag($tags[$tagNumber]);
+                    }
+                }
+            }
             $manager->persist($video);
         }
 
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [TagFixtures::class];
     }
 }
